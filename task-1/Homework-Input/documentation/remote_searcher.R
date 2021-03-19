acb1245b25967e11f6747de3a00538ad4de17f94
jiriklepl <jiriklepl@seznam.cz>
2021-03-19 07:50:34 +0100

    added new scripts

--- task-1/Homework-Input/documentation/remote_searcher.R
+++ task-1/Homework-Input/documentation/remote_searcher.R
@@ -5,10 +5,11 @@ library(tidyverse)
 library(dplyr)
 library(ggplot2)
 
-files <- list.files(pattern="local_measurement_\\d+_\\d+")
+files <- list.files(pattern="remote_searcher_\\d+_\\d+")
 per_edges <- c()
 distance_counts <- c()
 distance_total <- c()
+remote_distance_total <- c()
 for (file in files) {
     numbers <- as.numeric(str_extract_all(file, '\\d+')[[1]])
     file_data <- read_table(file)
@@ -26,17 +27,27 @@ for (file in files) {
         distance_total[file_data$Distance[i]] <- file_data$Time[i]
         if (!is.na(t) && length(t) > 0)
             distance_total[file_data$Distance[i]] <- t + distance_total[file_data$Distance[i]]
+
+        t <- remote_distance_total[file_data$Distance[i]]
+        remote_distance_total[file_data$Distance[i]] <- file_data$RTime[i]
+        if (!is.na(t) && length(t) > 0)
+            remote_distance_total[file_data$Distance[i]] <- t + remote_distance_total[file_data$Distance[i]]
     }
 
     per_edges$Time[length(per_edges$Time) + 1] <- sum(file_data$Time) / length(file_data$Time)
     per_edges$Density[length(per_edges$Density) + 1] <- 2 * numbers[2] / (numbers[1] * (numbers[1] - 1))
+    per_edges$RTime[length(per_edges$RTime) + 1] <- sum(file_data$RTime) / length(file_data$RTime)
 }
 
 per_edges <- per_edges %>% as_tibble %>% arrange(Density)
-per_edges %>% ggplot(aes(x=Density, y=Time)) + geom_point()
-ggsave("local_measurements1.png")
+per_edges %>% ggplot(aes(x=Density)) + (geom_point(aes(y=Time, color="Time"))) + (geom_point(aes(y=RTime, color="Remote time")))
+ggsave("remote_searcher1.png")
 
 distance_average <- ifelse(distance_counts > 0, distance_total / distance_counts, NA)
-png("local_measurements2.png")
+remote_distance_average <- ifelse(distance_counts > 0, remote_distance_total / distance_counts, NA)
+png("remote_searcher2_1.png")
 plot(x=(1:(distance_average %>% length)) - 1, y=distance_average, xlab="Distance (0 denotes infinity)", ylab="Average time")
+dev.off()
+png("remote_searcher2_2.png")
+plot(x=(1:(distance_average %>% length)) - 1, y=remote_distance_average, xlab="Distance (0 denotes infinity)", ylab="Average remote time")
 dev.off()
--- task-1/Homework-Input/documentation/scripts.sh
+++ task-1/Homework-Input/documentation/scripts.sh
@@ -1,9 +1,23 @@
 # for the step 1 of the homework assignment
-# to be run from the root directory
+# to be run from the root directory (no longer reliable as the implementation changed)
 local_measurement() {
     for EDGES in $(seq 100)
     do
         EDGES=$((1000 * (10 * EDGES - 8) / 2))
         bash run-client 1000 $EDGES > "documentation/local_measurement_1000_$EDGES"
     done
+}
+
+# for the step 2 of the homework assignment
+# to be run from the root directory 
+remote_searcher() {
+    bash run-server &
+    to_kill=$!
+    sleep 20
+    for EDGES in $(seq 100)
+    do
+        EDGES=$((200 * (2 * EDGES - 1) / 2))
+        bash run-client 200 $EDGES > "documentation/remote_searcher_200_$EDGES"
+    done
+    kill -9 $to_kill
 }
