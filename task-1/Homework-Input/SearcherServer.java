8106baf2ad1743fa3d18445c875564bac9376cd1
jiriklepl <jiriklepl@seznam.cz>
2021-03-21 00:36:35 +0100

    now prepared

--- task-1/Homework-Input/SearcherServer.java
+++ task-1/Homework-Input/SearcherServer.java
@@ -4,6 +4,8 @@ public class SearcherServer
 {
 	public static void main(String[] args){
 		try {
+			String host = args.length > 0 ? args[0] : "localhost";
+
 			// Instantiate the remotely accessible object. The constructor
 			// of the object automatically exports it for remote invocation.
 			Searcher searcher = new RemoteSearcherImpl();
@@ -12,8 +14,8 @@ public class SearcherServer
 			// Use the registry on this host to register the server.
 			// The host name must be changed if the server uses
 			// another computer than the client!
-			Naming.rebind(SearcherCommon.searcherName, searcher);
-			Naming.rebind(SearcherCommon.nodeFactoryName, nodeFactory);
+			Naming.rebind(SearcherCommon.searcherName(host), searcher);
+			Naming.rebind(SearcherCommon.nodeFactoryName(host), nodeFactory);
 
 			// The virtual machine will not exit here because the export of
 			// the remotely accessible object creates a new thread that
