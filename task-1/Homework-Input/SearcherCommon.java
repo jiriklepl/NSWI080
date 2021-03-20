6b5cda323a75e527d421058a6a0cd3c1aa0c0faf
jiriklepl <jiriklepl@seznam.cz>
2021-03-20 03:53:18 +0100

    finished step 5

--- task-1/Homework-Input/SearcherCommon.java
+++ task-1/Homework-Input/SearcherCommon.java
@@ -1,4 +1,4 @@
 public final class SearcherCommon {
-    public static final String searcherName = "//localhost/SearcherServer";
-    public static final String nodeFactoryName = "//localhost/NodeFactory";
+    public static final String searcherName = "//195.113.21.130/SearcherServer";
+    public static final String nodeFactoryName = "//195.113.21.130/NodeFactory";
 }
